e0f4b50915de1eed2b38ce661d9e17f1a6c23fa2
VladimirBoychenko <boichenkova6644@icloud.com>
2022-12-14 10:43:22 +0300

    add Example005_If_Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ведите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() ==  "masha")
+{
+    Console.WriteLine ("Ура, этоже Маша!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
